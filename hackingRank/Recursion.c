6ead03a4d2a7fa817e742891d0dae69102371dfa
YunJinHan <yjh9234@naver.com>
2017-02-10 13:01:38 +0900

    https://www.hackerrank.com/

---
+++ hackingRank/Recursion.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int factorial(int n);
+
+int main(void) {
+	int n;
+	scanf("%d",&n);
+	printf("%d",factorial(n));
+	return 0;
+}
+
+int factorial(int n) {
+	if (n<=1) {
+		return 1;
+	} else {
+		return n*factorial(n-1);
+	}
+}
