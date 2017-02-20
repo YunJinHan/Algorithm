947ec2ac4e400acc6509f4368234906c472d45a4
YunJinHan <yjh9234@gmail.com>
2017-02-20 12:56:50 +0900

    https://www.hackerrank.com/

---
+++ hackingRank/Interfaces.java
@@ -0,0 +1,40 @@
+import java.io.*;
+import java.util.*;
+
+interface AdvancedArithmetic {
+	int divisorSum(int n);
+}
+
+class Calculator implements AdvancedArithmetic {
+	public int divisorSum(int n) {
+		int sum = 1;
+		if (n == 1) return sum;
+		for (int i = 1; i <= Math.sqrt(n); i ++) {
+			if (i == 1) {
+				sum += n;
+			} else if (n%i == 0) {
+				if (i == n/i) {
+					sum += i;
+				} else {
+					sum += (i + n/i);
+				}
+			}
+		}
+		return sum;
+	}
+}
+
+
+class Solution {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int n = scan.nextInt();
+        scan.close();
+        
+      	AdvancedArithmetic myCalculator = new Calculator(); 
+        int sum = myCalculator.divisorSum(n);
+        System.out.println("I implemented: " + myCalculator.getClass().getInterfaces()[0].getName() );
+        System.out.println(sum);
+    }
+}
